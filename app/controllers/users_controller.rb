7eeab1ec15c9108cee941de6618524e492ff02f4
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-05-19 17:04:01 -0400

    added sign up flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,16 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params['id'])
   end
 
   def create
+    @user = User.create(user_params)
+    redirect_to user_url(@user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   get 'home', to: 'home#show'
+  root :to => redirect('/home')
 
   resources :users
   resources :posts
